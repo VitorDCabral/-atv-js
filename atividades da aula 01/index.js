df2bfbc22c753db201b41a8f68fa12561ff9751d
VitorDCabral <168486899+VitorDCabral@users.noreply.github.com>
2024-05-06 18:51:22 -0300

    versão 0.0.1
    A primeira atividade em sala da escola infinity school, com o professor Luan

---
+++ atividades da aula 01/index.js
@@ -0,0 +1,25 @@
+var name = promp("Qual o seu nome?")
+var age = promp("Qual sua idade?")
+
+// let dutra = {
+//   name : name,
+//   age : age,
+//   isHuman: true
+// }
+
+// console.log(`Olá, ${dutra.name} você tem ${dutra.age} anos`)
+
+let vitor = [
+  "labrador",
+  "pinscher",
+33,
+"borracha",
+{
+  name: "Helena",
+  age: 5,
+  isAdm: true
+},
+true
+]
+
+console.log(`A segunda linha do seu Array é ${vitor[4].age}`)
